a96830f040db5e7301185c0c61ad4cb6f877d6bf
Will Frame <william.frame@bastionsecurity.co.nz>
2024-05-02 13:16:36 +1200

    fixed filename parsing for pdfs

--- js2pdf.py
+++ js2pdf.py
@@ -1,11 +1,11 @@
 import argparse
-import os
+from os import path
 from PyPDF2 import PdfWriter, PdfReader
 
 parser = argparse.ArgumentParser()
 parser.add_argument('pdf', help='path to the input PDF file.')
 parser.add_argument('javascript', help='path to the input JavaScript file.')
-parser.add_argument('-o', '--output', help='specify an output PDF filename. by default, the new PDF is named using the old filename prefixed with "new_".')
+parser.add_argument('-o', '--output', help='specify an output PDF filename. by default, the new PDF is placed in the working directory named using the old filename prefixed with "new_".')
 parser.add_argument('-w', '--overwrite', action='store_true', help='overwrite the input PDF. This option ignores the -o/--output option.')
 args = parser.parse_args()
 if args.overwrite and args.output: parser.error("the -w/--overwrite option is incompatible with the -o/--output option.")
@@ -14,7 +14,7 @@ reader = PdfReader(open(args.pdf, 'rb'))
 script = open(args.javascript, 'r').read()
 if args.overwrite: filename = args.pdf
 elif args.output: filename = args.output
-else: filename = os.path.dirname(args.pdf) + "/new_" + os.path.basename(args.pdf)
+else: filename = "new_" + path.basename(args.pdf)
 
 output = PdfWriter()
 output.append_pages_from_reader(reader)
